0b24f7690d19bb38f92efa96d03006eef6b1dfca
wickidcow <jaywago@gmail.com>
2026-08-12 13:55:02 -0400

    Restore Giant Slime surprise to its correct source class

--- src/main/java/io/github/thebusybiscuit/slimefunluckyblocks/surprises/unlucky/GiantSlimeSurprise.java
+++ src/main/java/io/github/thebusybiscuit/slimefunluckyblocks/surprises/unlucky/GiantSlimeSurprise.java
@@ -3,28 +3,33 @@ package io.github.thebusybiscuit.slimefunluckyblocks.surprises.unlucky;
 import java.util.Random;
 
 import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Slime;
 
 import io.github.thebusybiscuit.slimefunluckyblocks.surprises.LuckLevel;
 import io.github.thebusybiscuit.slimefunluckyblocks.surprises.Surprise;
+import io.github.thebusybiscuit.slimefunluckyblocks.util.RegionTasks;
 
 public final class GiantSlimeSurprise implements Surprise {
-	
-	@Override
-	public String getName() {
-		return "Cobwebs";
-	}
 
-	@Override
-	public void activate(Random random, Player p, Location l) {
-		p.getLocation().getBlock().setType(Material.COBWEB);
-		p.getEyeLocation().getBlock().setType(Material.COBWEB);
-	}
+    @Override
+    public String getName() {
+        return "Giant Slime";
+    }
 
-	@Override
-	public LuckLevel getLuckLevel() {
-		return LuckLevel.UNLUCKY;
-	}
+    @Override
+    public void activate(Random random, Player player, Location location) {
+        Location spawn = location.clone();
+        RegionTasks.execute(spawn, () -> {
+            Slime slime = (Slime) spawn.getWorld().spawnEntity(spawn, EntityType.SLIME);
+            slime.setSize(7);
+            slime.setTarget(player);
+        });
+    }
 
+    @Override
+    public LuckLevel getLuckLevel() {
+        return LuckLevel.UNLUCKY;
+    }
 }
