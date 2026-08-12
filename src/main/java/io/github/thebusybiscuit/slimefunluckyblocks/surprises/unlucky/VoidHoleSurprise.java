89af5cdaf8529b782f11ac2b3467f53c08a4c55f
wickidcow <jaywago@gmail.com>
2026-08-12 13:54:37 -0400

    Fix Void Hole depth and Folia region safety

--- src/main/java/io/github/thebusybiscuit/slimefunluckyblocks/surprises/unlucky/VoidHoleSurprise.java
+++ src/main/java/io/github/thebusybiscuit/slimefunluckyblocks/surprises/unlucky/VoidHoleSurprise.java
@@ -4,32 +4,45 @@ import java.util.Random;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 import io.github.thebusybiscuit.slimefunluckyblocks.surprises.LuckLevel;
 import io.github.thebusybiscuit.slimefunluckyblocks.surprises.Surprise;
+import io.github.thebusybiscuit.slimefunluckyblocks.util.RegionTasks;
 
 public final class VoidHoleSurprise implements Surprise {
-	
-	@Override
-	public String getName() {
-		return "Void Hole";
-	}
 
-	@Override
-	public void activate(Random random, Player p, Location l) {
-		for (int x = p.getLocation().getBlockX() - 1; x <= p.getLocation().getBlockX() + 1; x++) {
-			for (int z = p.getLocation().getBlockZ() - 1; z <= p.getLocation().getBlockZ() + 1; z++) {
-				for (int y = p.getLocation().getBlockY() + 1; y >= 0; y--) {
-					l.getWorld().getBlockAt(x, y, z).setType(Material.AIR);
-				}
-			}
-		}
-	}
+    @Override
+    public String getName() {
+        return "Void Hole";
+    }
 
-	@Override
-	public LuckLevel getLuckLevel() {
-		return LuckLevel.UNLUCKY;
-	}
+    @Override
+    public void activate(Random random, Player player, Location location) {
+        Location origin = player.getLocation();
+        World world = origin.getWorld();
 
+        int centerX = origin.getBlockX();
+        int centerZ = origin.getBlockZ();
+        int topY = Math.min(origin.getBlockY() + 1, world.getMaxHeight() - 1);
+        int minY = world.getMinHeight();
+
+        RegionTasks.executeColumns(
+                world,
+                centerX - 1,
+                centerX + 1,
+                centerZ - 1,
+                centerZ + 1,
+                (x, z) -> {
+                    for (int y = topY; y >= minY; y--) {
+                        world.getBlockAt(x, y, z).setType(Material.AIR, false);
+                    }
+                });
+    }
+
+    @Override
+    public LuckLevel getLuckLevel() {
+        return LuckLevel.UNLUCKY;
+    }
 }
